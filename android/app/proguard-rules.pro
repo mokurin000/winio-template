1756200ecdadfc552b46a3b88a55dd47ae7ebc28
莯凛 <mokurin000@gmail.com>
2026-06-30 20:27:35 +0800

    feat: basic counter app

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -6,40 +6,31 @@
 -keep, includedescriptorclasses class com.google.androidgamesdk.** { *; }
 -keep, includedescriptorclasses class androidx.activity.result.** { *; }
 
+# Button
+-keep, includedescriptorclasses class com.google.android.material.button.** { *; }
+
+# CheckBox
+# -keep, includedescriptorclasses class com.google.android.material.checkbox.** { *; }
+
+# RadioButton
+# -keep, includedescriptorclasses class com.google.android.material.radiobutton.** { *; }
+
 # Media
-# Uncomment if using the Media component.
 # -keep, includedescriptorclasses class androidx.media3.** { *; }
 
 # LinkLabel
-# Uncomment if using the LinkLabel component.
 # -keep, includedescriptorclasses class rs.compio.winio.ClickableSpan { *; }
 
 # TabView
-# Uncomment if using the TabView component.
 # -keep, includedescriptorclasses class rs.compio.winio.TabViewAdapter { *; }
 # -keep, includedescriptorclasses class com.google.android.material.tabs.** { *; }
 # -keep, includedescriptorclasses class androidx.viewpager2.widget.** { *; }
 
 # WebView
-# Uncomment if using the WebView component.
 # -keep, includedescriptorclasses class rs.compio.winio.WebViewClient { *; }
 
-# Button
-# Uncomment if using the Button component.
-# -keep, includedescriptorclasses class com.google.android.material.button.** { *; }
-
-# CheckBox
-# Uncomment if using the CheckBox component.
-# -keep, includedescriptorclasses class com.google.android.material.checkbox.** { *; }
-
-# RadioButton
-# Uncomment if using the RadioButton component.
-# -keep, includedescriptorclasses class com.google.android.material.radiobutton.** { *; }
-
 # Progress
-# Uncomment if using the Progress component.
 # -keep, includedescriptorclasses class com.google.android.material.progressindicator.** { *; }
 
 # Slider
-# Uncomment if using the Slider component.
 # -keep, includedescriptorclasses class com.google.android.material.slider.** { *; }
--- src/model.rs
+++ src/model.rs
@@ -8,6 +8,14 @@ use crate::Result;
 pub struct MainModel {
     /// The main application window.
     window: Child<Window>,
+    /// label
+    label: Child<Label>,
+    /// count up
+    countup: Child<Button>,
+    /// count down
+    countdown: Child<Button>,
+
+    count: i8,
 }
 
 pub enum MainMessage {
@@ -19,6 +27,10 @@ pub enum MainMessage {
     ThemeChanged,
     /// Close main window
     Close,
+    /// Count plus by one
+    CountUp,
+    /// Count minus by one
+    CountDown,
 }
 
 impl Component for MainModel {
@@ -34,16 +46,32 @@ impl Component for MainModel {
         init! {
             window: Window = (()) => {
                 text: "Main window",
-                size: Size::new(800.0, 600.0),
+                size: Size::new(300.0, 100.0),
 
                 #[cfg(all(windows, feature = "winui"))]
                 backdrop: Backdrop::Mica,
+            },
+            label: Label = (&window) => {
+                text: "0",
+            },
+            countup: Button = (&window) => {
+                text: "+",
+            },
+            countdown: Button = (&window) => {
+                text: "-",
             },
         }
 
         window.show()?;
 
-        Ok(Self { window })
+        Ok(Self {
+            window,
+            label,
+            countup,
+            countdown,
+
+            count: 0,
+        })
     }
 
     async fn start(&mut self, sender: &ComponentSender<Self>) -> ! {
@@ -55,12 +83,18 @@ impl Component for MainModel {
                 WindowEvent::Close => MainMessage::Close,
                 WindowEvent::ThemeChanged => MainMessage::ThemeChanged,
             },
+            self.countup => {
+                ButtonEvent::Click => MainMessage::CountUp,
+            },
+            self.countdown => {
+                ButtonEvent::Click => MainMessage::CountDown,
+            },
         }
     }
 
     async fn update_children(&mut self) -> Result<bool> {
-        // update the window
-        update_children!(self.window)
+        // update the window and functional children
+        update_children!(self.window, self.countup,)
     }
 
     async fn update(
@@ -71,21 +105,49 @@ impl Component for MainModel {
         // deal with custom messages
         match message {
             MainMessage::Noop => Ok(false),
-            MainMessage::Resize => Ok(false),
             MainMessage::ThemeChanged => Ok(false),
+            MainMessage::Resize => Ok(true),
             MainMessage::Close => {
                 // the root component output stops the application
                 sender.output(());
                 // need not to call `render`
                 Ok(false)
             }
+            MainMessage::CountUp => {
+                self.count += 1;
+                self.label.set_text(format!("{}", self.count))?;
+                Ok(true)
+            }
+            MainMessage::CountDown => {
+                self.count -= 1;
+                self.label.set_text(format!("{}", self.count))?;
+                Ok(true)
+            }
         }
     }
 
     fn render(&mut self, _sender: &ComponentSender<Self>) -> Result<()> {
-        let _csize = self.window.client_size()?;
+        let csize = self.window.client_size()?;
 
-        // add your UI layout here
+        let mut buttons = layout! {
+            StackPanel::new(Orient::Horizontal),
+            self.countup => {
+                halign: HAlign::Center,
+            },
+            self.countdown => {
+                halign: HAlign::Center,
+            },
+        };
+        let mut layout = layout! {
+            StackPanel::new(Orient::Vertical),
+            self.label => {
+                halign: HAlign::Center,
+            },
+            buttons => {
+                halign: HAlign::Center,
+            },
+        };
+        layout.set_size(csize)?;
         Ok(())
     }
 
